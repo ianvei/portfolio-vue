f0b84bf24ede37084b362c6c127e8d78cf6020f3
ianvei <iamianveilleux@gmail.com>
2022-08-29 16:49:09 -0400

    https

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'www.ianveilleux.ca',
+  base: 'https://www.ianveilleux.ca/',
   plugins: [vue()],
 
 })
